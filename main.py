2f29403ba7c24a73ad625a34e482afd67863eb8d
joelrobin18 <joelrobin1818@gmail.com>
2023-01-23 19:51:33 +0530

    Get post with specific id and latest post added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI,Body,HTTPException,Response,status
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+from data import *
 app=FastAPI()
 
 ## Schema Validation
@@ -11,38 +12,46 @@ class Post(BaseModel):
     author:str
     rating:Optional[float]=None
 
+
 ## Saving your data/ Database is not included now
 ## On refreshing the data get changed. So its not stored permenantly
-posts=[ {
-            "title": "Test Post 1",
-            "caption": "Test Caption 1",
-            "author": "Test 1",
-            "rating": 4.2,
-            "id": 1
-        },
-        {
-            "title": "Test Post 2",
-            "caption": "Test Caption 2",
-            "author": "Test 3",
-            "rating": 5.6,
-            "id": 2
-        },
-        {
-            "title": "Test Post 3",
-            "caption": "Test Caption 2",
-            "author": "Test 3",
-            "rating": 10.0,
-            "id": 3
-        }]
+
+# Function for finding the post with particular id
+def find_post(id):
+    for i in posts:
+        if i['id']==id:
+            return i
 
 ## To make get request to the server. To get the data
 @app.get("/posts") 
 def root():
-    return {"Post":posts[2]['title']}
+    return {"Post":posts}
 
+
+# Creating a new post
 @app.post("/posts",status_code=status.HTTP_201_CREATED) ## Status Code Changed for creating a new post
 def create(post:Post):
     post=post.dict()
     post['id']=randrange(0,10000)
     posts.append(post)
     return {"All Post":posts}
+
+
+# Get the latest Post
+@app.get("/posts/latest")
+def latest():
+    latest_posts=[]
+    latest_posts.append(posts[len(posts)-1])
+    latest_posts.append(posts[len(posts)-2])
+    return {"Latest Post":latest_posts}
+
+# Getting a particular post 
+@app.get("/posts/{id}")
+def get_post(id:int):
+    post= find_post(id)
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                        detail=f"The post with id : {id} is not found")
+    
+    return {"message":post}
