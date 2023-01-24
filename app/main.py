15fe6aecb56ea3aa87fca3a52239b0d43655a44f
joelrobin18 <joelrobin1818@gmail.com>
2023-01-24 23:45:47 +0530

    Database added and retrieved all datas

--- app/main.py
+++ app/main.py
@@ -2,14 +2,30 @@ from fastapi import FastAPI,Body,HTTPException,Response,status
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
-from data import *
+from app.data import *
+import psycopg2
+import time ## Timer for sleep for or wait for certain amount of time
 app=FastAPI()
+
+
+try:
+    conn=psycopg2.connect(host="blaaaa",
+                            database="blaaaaa", 
+                            user="blaaaa",
+                            password="1234567890")
+    
+    cursor=conn.cursor()
+    print("Database Successfully Connected")
+except Exception as error:
+    print("Error",error)
+    pass
 
 ## Schema Validation
 class Post(BaseModel):
     title:str
     caption:str
     author:str
+    published:bool = False
     rating:Optional[float]=None
 
 
@@ -29,6 +45,8 @@ def find_post_index(id,i):
 ## To make get request to the server. To get the data
 @app.get("/posts") 
 def root():
+    cursor.execute("""select * from post """)
+    posts = cursor.fetchall()
     return {"Post":posts}
 
 
@@ -62,15 +80,14 @@ def get_post(id:int):
     return {"message":post}
 
 #Delete all the post
-@app.delete("/posts/delete/all")
+@app.delete("/posts/delete/all",status_code=status.HTTP_204_NO_CONTENT)
 def delete_all_post():
     posts.clear()
-    return {"Message":"Post Deleted Successfully",
-            "Posts":posts}
+    return Response(status_code=status.HTTP_204_NO_CONTENT) # While deleting a content we dont need to return any response. So we return no content as response
 
 
 #Delete a post with particular id
-@app.delete("/posts/delete/{id}")
+@app.delete("/posts/delete/{id}",status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id:int):
     i=-1
     deletepost,index=find_post_index(id,i)
@@ -79,8 +96,20 @@ def delete_post(id:int):
                         detail=f"The post with id : {id} is not found")
     
     posts.pop(index)
-    return {"Message":"Post Deleted Successfully",
-            "Post":posts}
-    
-    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
+## Updating a post as completely
+@app.put("/posts/{id}")
+def update(id:int,post:Post):
+    i=-1
+    post_value,index=find_post_index(id,i)
+    
+    if index==-1:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"Post with id {id} not found")
+    
+    updated_post=post.dict() # Convert the class into dict
+    updated_post['id']=id # Store the data with the same id
+    posts[index]=updated_post # Store the same data into the same index
+    
+    return {"message":posts}
